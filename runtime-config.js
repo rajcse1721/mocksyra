2ea99a26e4cf02c6953bfac0bf45d323cf0028a0
RS096293 <Rajesh.Singh@cerner.com>
2026-09-13 22:11:30 +0530

    Connect frontend to Mocksyra realtime server

--- runtime-config.js
+++ runtime-config.js
@@ -1,3 +1,3 @@
 // Local development leaves this blank and connects to the same origin.
 // After Render deploys, replace this value with the HTTPS Render service URL.
-window.MOCKSYRA_SOCKET_URL = '';
+window.MOCKSYRA_SOCKET_URL = "https://mocksyra-realtime.onrender.com";
